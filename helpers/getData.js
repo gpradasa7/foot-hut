707e553bb763637c070406f7edb6737278b0dfcf
Geiler Prada <geilerprada7@gmail.com>
2022-05-21 00:00:27 -0500

    change colors

--- helpers/getData.js
+++ helpers/getData.js
@@ -1,6 +1,6 @@
 const GetData = async url => {
   let { data } = await axios.get(url);
-  console.log(data);
+
   return data;
 };
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,57 @@
 import { showData } from "../modules/showData.js";
 import GetData from "../helpers/getData.js";
+
 const ramenButton = document.getElementById("ramen");
-const breakfast = document.getElementById("breakfast");
-const lunch = document.getElementById("lunch");
-const dinner = document.getElementById("dinner");
-const mexican = document.getElementById("mexican");
-const italian = document.getElementById("italian");
-const desserts = document.getElementById("desserts");
-const drinks = document.getElementById("drinks");
+const breakfastButton = document.getElementById("breakfast");
+const lunchButton = document.getElementById("lunch");
+const dinnerButton = document.getElementById("dinner");
+const mexicanButton = document.getElementById("mexican");
+const italianButton = document.getElementById("italian");
+const dessertsButton = document.getElementById("desserts");
+const drinksButton = document.getElementById("drinks");
 const dataRamen = "http://localhost:3000/ramen";
+const databreakfast = "http://localhost:3000/breakfast";
+const datalunch = "http://localhost:3000/lunch";
+const datadinner = "http://localhost:3000/dinner";
+const datamexican = "http://localhost:3000/mexican";
+const dataitalian = "http://localhost:3000/italian";
+const datadesserts = "http://localhost:3000/desserts";
+const datadrinks = "http://localhost:3000/drinks";
 
 const container = document.getElementById("containerCards");
-
+const main2 = document.querySelector(".main2");
 document.addEventListener("DOMContentLoaded", async () => {
   let ramen = await GetData(dataRamen);
-  console.log(ramen);
+  let breakfast = await GetData(databreakfast);
+  let lunch = await GetData(datalunch);
+  let dinner = await GetData(datadinner);
+  let mexican = await GetData(datamexican);
+  let italian = await GetData(dataitalian);
+  let desserts = await GetData(datadesserts);
+  let drinks = await GetData(datadrinks);
+
   ramenButton.addEventListener("click", async () => {
     showData(ramen, container);
+  });
+  breakfastButton.addEventListener("click", async () => {
+    showData(breakfast, container);
+  });
+  lunchButton.addEventListener("click", async () => {
+    showData(lunch, container);
+  });
+  dinnerButton.addEventListener("click", async () => {
+    showData(dinner, container);
+  });
+  mexicanButton.addEventListener("click", async () => {
+    showData(mexican, container);
+  });
+  italianButton.addEventListener("click", async () => {
+    showData(italian, container);
+  });
+  dessertsButton.addEventListener("click", async () => {
+    showData(desserts, container);
+  });
+  drinksButton.addEventListener("click", async () => {
+    showData(drinks, container);
   });
 });
